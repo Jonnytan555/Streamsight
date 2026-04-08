ee082a2dd111afa5981a97b31780fb95cb96d6dc
Winter1963# <jonnyedwards1@hotmail.co.uk>
2026-04-08 17:26:31 +0100

    Update databases

--- runners/db/ensog/appsettings.py
+++ runners/db/ensog/appsettings.py
@@ -11,7 +11,7 @@ COMMODITY_GROUP          = "Natural Gas"
 COMMODITY_CLASSIFICATION = "European Gas"
 COMMODITY_NAME           = None
 
-SCRAPE_DB_SERVER = "PRD-DB-SQL-208" if os.getenv("SERVER") == "PRD" else "localhost"
+SCRAPE_DB_SERVER = "localhost"
 
 DB_ENGINE  = build_engine(server=SCRAPE_DB_SERVER, database="Scrape")
 SOURCE_URL = f"mssql://{SCRAPE_DB_SERVER}/Scrape/dbo/{SOURCE_TABLE}"
